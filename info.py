7a97eaac95925adfb1f02f3be6da5ac870158681
Jeff Hykin <jeff.hykin@gmail.com>
2022-03-10 10:58:22 -0600

    add config to fix the missing file problems

---
+++ info.py
@@ -0,0 +1,20 @@
+from quik_config import find_and_load
+
+# returns more than just config
+info = find_and_load("info.yaml", default_options=[])
+config                = info.config         # the resulting dictionary for all the selected options
+path_to               = info.path_to               # a dictionary of paths relative to the root_path
+absolute_path_to      = info.absolute_path_to      # same dictionary of paths, but made absolute
+project               = info.project               # the dictionary to everything inside (project)
+root_path             = info.root_path             # parent folder of the .yaml file
+configuration_choices = info.configuration_choices # the dictionary of the local config-choices files
+configuration_options = info.configuration_options # the dictionary of all possible options
+as_dict               = info.as_dict               # the dictionary to the whole file (info.yaml)
+
+# 
+# create any missing folders
+# 
+for each_name in path_to.folders:
+    each_path = path_to.folders[each_name]
+    import os
+    os.makedirs(each_path, exist_ok=True)
--- train_agent.py
+++ train_agent.py
@@ -2,5 +2,7 @@ import gym
 
 from stable_baselines3 import SAC
 
-for env_name in ["LunarLanderContinuous-v2", "MountainCarContinuous-v0"]:
-    SAC("MlpPolicy", env_name, verbose=2).learn(100_000).save(f'data/{env_name}')
+from info import path_to, config
+
+for env_name in config.env_names:
+    SAC("MlpPolicy", env_name, verbose=2).learn(100_000).save(f'{path_to.folders.agent_models}{env_name}')
