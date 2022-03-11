e89b9f7ac65831e76908a7055e48d6e115996c9c
Jeff Hykin <jeff.hykin@gmail.com>
2022-03-11 14:24:37 -0600

    update config version

--- info.py
+++ info.py
@@ -3,21 +3,17 @@ from quik_config import find_and_load, LazyDict
 # 
 # load the options
 # 
-info = find_and_load("info.yaml", default_options=[])
+info = find_and_load("info.yaml")
 config                = info.config         # the resulting dictionary for all the selected options
 path_to               = info.path_to               # a dictionary of paths relative to the root_path
 absolute_path_to      = info.absolute_path_to      # same dictionary of paths, but made absolute
-project               = info.project               # the dictionary to everything inside (project)
-root_path             = info.root_path             # parent folder of the .yaml file
-configuration_choices = info.configuration_choices # the dictionary of the local config-choices files
-configuration_options = info.configuration_options # the dictionary of all possible options
-as_dict               = info.as_dict               # the dictionary to the whole file (info.yaml)
 
 # 
 # set torch device
 # 
+import torch
+config.device = torch.device('cpu')
 if config.has_gpu:
-    import torch
     config.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 # 
@@ -33,6 +29,7 @@ for each_name in path_to.folder:
 # 
 path_to.agent_model_for    = lambda env_name: f"{path_to.folder.agent_models}/{env_name}"
 path_to.dynamics_model_for = lambda env_name: f"{path_to.folder.dynamics_models}/{env_name}.pt"
+path_to.experiment_csv_for = lambda env_name: f"{path_to.folder.results}/{env_name}/experiments_pp.csv"
 
 # 
 # env lookup
