086cb88a50aed675e89eae6be765cab0254b05d8
Jeff Hykin <jeff.hykin@gmail.com>
2022-03-11 17:26:48 -0600

    attempt device mismatch fix

--- mlp.py
+++ mlp.py
@@ -1,8 +1,10 @@
 import numpy as np
-import torch as th
+import torch as torch
 from torch import FloatTensor as ft
 from torch import nn
 from torch.optim import Adam
+
+from info import config
 
 
 def mlp(sizes, activation=nn.Tanh, output_activation=nn.Identity):
@@ -16,7 +18,7 @@ def mlp(sizes, activation=nn.Tanh, output_activation=nn.Identity):
 
 class DynamicsModel(nn.Module):
     # State transition dynamics model
-    def __init__(self, obs_dim, act_dim, hidden_sizes, lr, device):
+    def __init__(self, obs_dim, act_dim, hidden_sizes, lr, device=config.device):
         super().__init__()
         self.device = device
         self.model = mlp([obs_dim + act_dim, *hidden_sizes, obs_dim], nn.ReLU).to(
@@ -28,15 +30,15 @@ class DynamicsModel(nn.Module):
         # used for a single state and action
         obs = ft(obs).to(self.device)
         act = ft(act).to(self.device)
-        with th.no_grad():
-            next_observation = self.model(th.cat((obs, act), -1))
+        with torch.no_grad():
+            next_observation = self.model(torch.cat((obs, act), -1))
         return next_observation.cpu().numpy()
 
-    def predict(self, obs: th.Tensor, act: th.Tensor):
+    def predict(self, obs: torch.Tensor, act: torch.Tensor):
         # used for batch predictions
         # expecting obs and act to be on device
         # returns the predictions still on the device
-        return self.model(th.cat((obs, act), -1))
+        return self.model(torch.cat((obs, act), -1).to(self.device))
 
 
 class RobustPredictivePolicy(nn.Module):
@@ -45,7 +47,7 @@ class RobustPredictivePolicy(nn.Module):
         self.model = mlp([obs_dim, *hidden_sizes, act_dim], nn.ReLU)
         self.optimizer = Adam(self.model.parameters(), lr=lr)
 
-    def predict(self, obs: th.Tensor):
+    def predict(self, obs: torch.Tensor):
         return self.model(obs)
 
     def forward(self, obs):
