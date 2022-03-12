dd72a4e6a64d443b352b879dd63553154fc98fa8
Jeff Hykin <jeff.hykin@gmail.com>
2022-03-11 20:37:45 -0600

    fail to get gym render working

--- settings/during_start/005_000__setup_ld_path__.sh
+++ settings/during_start/005_000__setup_ld_path__.sh
@@ -3,3 +3,5 @@ export LD_LIBRARY_PATH="$("$__FORNIX_NIX_COMMANDS/lib_path_for" "cc"):$LD_LIBRAR
 # add these for opencv/numpy
 export LD_LIBRARY_PATH="$("$__FORNIX_NIX_COMMANDS/lib_path_for" libglvnd):$LD_LIBRARY_PATH"
 export LD_LIBRARY_PATH="$("$__FORNIX_NIX_COMMANDS/lib_path_for" glib):$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$("$__FORNIX_NIX_COMMANDS/lib_path_for" libGLU):$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$("$__FORNIX_NIX_COMMANDS/lib_path_for" xorg.libX11):$LD_LIBRARY_PATH"
