2bf8a1c6b3305da305e009e3f7fe571d9d1ea2fe
Jeff Hykin <jeff.hykin@gmail.com>
2022-04-04 15:08:08 -0500

    add plotting method

---
+++ main/scripts/generate_training_graphs.py
@@ -0,0 +1,7 @@
+from main.analysis import plot_epsilon_1, plot_epsilon_2
+from info import path_to, config
+
+env_names = config.env_names
+
+for each in env_names: plot_epsilon_1(each)
+for each in env_names: plot_epsilon_2(each)
